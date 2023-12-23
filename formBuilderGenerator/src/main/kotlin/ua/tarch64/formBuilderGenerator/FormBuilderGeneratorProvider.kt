8aad63cc794ddff7805c7988696f3bdc55fd6238
TArch64 <tarasturchenko1551@gmail.com>
2023-12-23 11:40:10 +0200

    refactor generator structure

--- formBuilderGenerator/src/main/kotlin/ua/tarch64/formBuilderGenerator/FormBuilderGeneratorProvider.kt
+++ formBuilderGenerator/src/main/kotlin/ua/tarch64/formBuilderGenerator/FormBuilderGeneratorProvider.kt
@@ -3,15 +3,17 @@ package ua.tarch64.formBuilderGenerator
 import com.google.devtools.ksp.processing.SymbolProcessor
 import com.google.devtools.ksp.processing.SymbolProcessorEnvironment
 import com.google.devtools.ksp.processing.SymbolProcessorProvider
-import ua.tarch64.formBuilderGenerator.formModel.FormObjectGenerator
+import ua.tarch64.formBuilderGenerator.factory.ClassSpecFactory
+import ua.tarch64.formBuilderGenerator.factory.FileSpecFactory
+import ua.tarch64.formBuilderGenerator.factory.FunSpecFactory
 
 class FormBuilderGeneratorProvider : SymbolProcessorProvider {
     override fun create(environment: SymbolProcessorEnvironment): SymbolProcessor {
         return FormBuilderGenerator(
-            formModelGenerator = FormObjectGenerator(
-                codeGenerator = environment.codeGenerator,
-                logger = environment.logger,
-            )
+            codeGenerator = environment.codeGenerator,
+            fileFactory = FileSpecFactory(),
+            classSpecFactory = ClassSpecFactory(),
+            funSpecFactory = FunSpecFactory()
         )
     }
 }
